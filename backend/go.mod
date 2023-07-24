74a9df1a3719adb4d5ba682def258b92b2863e54
Kalaiarasan Senthil <kalaiarasan70810@gmail.com>
2023-07-24 17:51:11 +0530

    base-setup

---
+++ backend/go.mod
@@ -0,0 +1,3 @@
+module github.com/3D-ShowCaser/backend
+
+go 1.20
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="3D Show Caser" />
+</svelte:head>
+
+<section>
+	3D Show Caser
+</section>
