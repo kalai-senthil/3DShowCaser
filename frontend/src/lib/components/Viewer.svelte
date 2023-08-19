82ee5046768d0d4978484ee0ba01eb83e85f5f6e
kalai-senthil <kalaiarasan70810@gmail.com>
2023-08-19 10:17:52 +0530

    options-add-ui-fix

--- frontend/src/lib/components/Viewer.svelte
+++ frontend/src/lib/components/Viewer.svelte
@@ -2,12 +2,7 @@
 	import { onMount } from 'svelte';
 	import { PUBLIC_BACKEND_URL } from '$env/static/public';
 	export let data: Work | null;
-	import px from '$lib/images/textures/px.png';
-	import py from '$lib/images/textures/py.png';
-	import pz from '$lib/images/textures/pz.png';
-	import nz from '$lib/images/textures/nz.png';
-	import nx from '$lib/images/textures/nx.png';
-	import ny from '$lib/images/textures/ny.png';
+	import Icon from '@iconify/svelte';
 	export let token: string;
 	import {
 		AmbientLight,
@@ -17,29 +12,37 @@
 		DoubleSide,
 		LoadingManager,
 		Mesh,
-		MeshBasicMaterial,
 		MeshStandardMaterial,
 		PerspectiveCamera,
 		Scene,
-		Triangle,
 		WebGLRenderer
 	} from 'three';
 	import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 	import type { Work } from '$lib/types';
 	let canvas: HTMLCanvasElement;
+	let lightIntensity = 1;
 	const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 	const scene = new Scene();
 	const loader = new GLTFLoader(new LoadingManager());
-	const light = new AmbientLight(new Color('white'));
-	light.intensity = 10;
+	const light = new AmbientLight(new Color('white'), lightIntensity);
+	let showSidePanel = false;
 	const textureLoader = new CubeTextureLoader();
-	loader.setPath('textures/cube/pisa/');
-	const textureCube = textureLoader.load([px, nx, py, ny, pz, nz]);
+	textureLoader.setPath('/textures/Bridge/');
+	let textureCube = textureLoader.load([
+		'px.jpg',
+		'nx.jpg',
+		'py.jpg',
+		'ny.jpg',
+		'pz.jpg',
+		'nz.jpg'
+	]);
+	let backgroundIntensity = 1;
 	onMount(() => {
 		scene.background = textureCube;
-		loader.setPath('');
+		scene.backgroundIntensity = backgroundIntensity;
 		loader.setWithCredentials(true);
+		loader.setPath('');
 		loader.load(`${PUBLIC_BACKEND_URL}/${data?.path}`, (gltf) => {
 			gltf.scene.traverse(function (node) {
 				if (node instanceof Mesh) {
@@ -54,8 +57,11 @@
 		});
 		const controls = new OrbitControls(camera, renderer.domElement);
 		controls.enableDamping = true;
+		controls.enableZoom = true;
 		function render() {
 			window.requestAnimationFrame(() => {
+				light.intensity = lightIntensity;
+				scene.backgroundIntensity = backgroundIntensity;
 				controls.update();
 				camera.updateProjectionMatrix();
 				renderer.render(scene, camera);
@@ -77,4 +83,66 @@
 	});
 </script>
 
-<canvas bind:this={canvas} />
+<section class="relative">
+	<div
+		class={`absolute flex top-20 transition-all ease-out items-start ${
+			showSidePanel ? 'right-0' : '-right-52'
+		}`}
+	>
+		<button
+			on:click={() => {
+				showSidePanel = !showSidePanel;
+			}}
+			class="bg-black rounded-l-md p-1 cursor-pointer mt-10"
+		>
+			<Icon
+				icon="bxs:up-arrow"
+				class={`duration-500 ${
+					showSidePanel ? 'rotate-90' : '-rotate-90'
+				} transition-transform text-2xl ease-in-out`}
+			/>
+		</button>
+		<section class="bg-black w-52 shadow-lg p-2 rounded-md">
+			<div class="flex flex-col">
+				<div class="flex">
+					<h4 class="grow">Light Intensity</h4>
+					<p class="border p-1 aspect-square align-text-bottom w-10 text-center">
+						{lightIntensity}
+					</p>
+				</div>
+				<input min="0" max="100" step="1" type="range" bind:value={lightIntensity} />
+			</div>
+			<div class="flex flex-col">
+				<div class="flex">
+					<h4 class="grow">Background Intensity</h4>
+					<p class="border p-1 aspect-square align-text-bottom w-10 text-center">
+						{backgroundIntensity}
+					</p>
+				</div>
+				<input min="0" max="10" step=".5" type="range" bind:value={backgroundIntensity} />
+			</div>
+			<div class="flex flex-col">
+				<h4 class="grow">Environment</h4>
+				<select
+					on:change={(e) => {
+						textureLoader.setPath(e.currentTarget.value);
+						textureCube = textureLoader.load([
+							'px.jpg',
+							'nx.jpg',
+							'py.jpg',
+							'ny.jpg',
+							'pz.jpg',
+							'nz.jpg'
+						]);
+						scene.background = textureCube;
+					}}
+				>
+					<option value="/textures/Bridge/">Bridge</option>
+					<option value="/textures/Yokahama/">Yokahama</option>
+					<option value="/textures/Storfrozen/">Storfrozen</option>
+				</select>
+			</div>
+		</section>
+	</div>
+	<canvas bind:this={canvas} />
+</section>
